a12c2b933bf0c285ebbb2e206e80592c90b5b9c0
Dausbel <dtorreblanca@integradoc.com>
2022-08-23 20:39:52 +0700

    first commit

---
+++ src/main/java/com/amsa/capacitacion/profesor/ProfesorArtifactApplication.java
@@ -0,0 +1,13 @@
+package com.amsa.capacitacion.profesor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfesorArtifactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfesorArtifactApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/amsa/capacitacion/profesor/ProfesorArtifactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amsa.capacitacion.profesor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfesorArtifactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
